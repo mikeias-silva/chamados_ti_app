df0921e77c0c360df3c533cc88870554a3076708
mikeias-silva <mikeias26@gmail.com>
2023-07-29 15:10:32 -0300

    rotas de situacoes, categorias e chamados

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
+use App\Http\Controllers\CategoriasController;
+use App\Http\Controllers\ChamadosController;
+use App\Http\Controllers\SituacoesController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'chamados', 'as' => 'chamados.'], function () {
+    Route::resource('/', ChamadosController::class);
+});
+
+Route::group(['prefix' => 'categorias', 'as' => 'categorias.'], function () {
+    Route::resource('/', CategoriasController::class);
+});
+
+Route::group(['prefix' => 'situacoes', 'as' => 'situacoes.'], function () {
+    Route::resource('/', SituacoesController::class);
+});
+Route::get('/store', [SituacoesController::class, 'store']);
